2f94e8afc772e2ec27a0e648774c013b69db738b
Mathezinho <00001098597114sp@al.educacao.sp.gov.br>
2024-08-15 17:16:14 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+let xb = 300;
+let nyb = 200;
+let vxb = 6;
+let vyb = 6;
+let diametro = 15;
+let raio = diametro/2;
+
+let xr = 5;
+let yr = 150;
+let ar = 80;
+let ir = 7;
+
+let xri = 585;
+let yri = 150;
+
+let colidiu false;
+
+let meuspontos = 0;
+let pontosdooponente = 0;
+
+
+let trilha;
+let raquetada;
+let ponto;
+
+function setup() {
+  createCanvas(600,400);
+  trilha.loop();
+}
+
+function setup() {
+  backgro
+}
+function preload(){
+  trilha = loadSound("trilha.mp3");
+  raquetada = loadSound("raquetada.mp3");
+  ponto = loadSound("ponto.mp3");
+}
